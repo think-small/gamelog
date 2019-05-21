be4f070f4158858c44f214abf3e603b7c7410ec2
think small <barry.t.small@gmail.com>
2019-05-21 19:55:35 +0000

    added app and db to init file

--- gamelog/__init__.py
+++ gamelog/__init__.py
@@ -1,1 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "209d17442547bb4fdd79b554d376e6b0"
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+print("It is working!")
